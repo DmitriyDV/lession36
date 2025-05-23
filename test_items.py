1997aef368360526730427fb226b3262ccf45759
DmitriyDV <31035451+DmitriyDV@users.noreply.github.com>
2025-05-24 00:32:30 +0300

    test check button submit

--- test_items.py
+++ test_items.py
@@ -1,1 +1,7 @@
+from selenium.webdriver.common.by import By
 
+def test_add_to_basket_button_is_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    button_add_to_basket = browser.find_element(By.XPATH, "//button[@value='Add to basket']")
+    assert button_add_to_basket, "Кнопка добавления в корзину не найдена"
