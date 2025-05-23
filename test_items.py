f3151280a54374bd7fe9cfbbfd4dcdcf756e6064
dmitriydrozhzhin <drozhzhindmitriy@icloud.com>
2025-05-24 02:17:06 +0300

    add implicitly wait and change path selector for button

--- test_items.py
+++ test_items.py
@@ -1,7 +1,11 @@
+import time
+from time import sleep
+
 from selenium.webdriver.common.by import By
 
 def test_add_to_basket_button_is_present(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button_add_to_basket = browser.find_element(By.XPATH, "//button[@value='Add to basket']")
+    browser.implicitly_wait(3)
+    button_add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button_add_to_basket, "Кнопка добавления в корзину не найдена"
